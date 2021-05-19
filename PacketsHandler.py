97b03e2c65c8bd6ec6aec1eb5dab9b5a4448f9d2
GiuseppeMilotta <milottag@gmail.com>
2021-05-19 12:26:53 +0200

    Update PacketsHandler.py

--- PacketsHandler.py
+++ PacketsHandler.py
@@ -65,9 +65,8 @@ def TypeReport(packet):
 
 
 def TypeData(packet,s):
-    print("SONO GIUSEPPE-----------------------------------------------------------------------")
-    #if (packet.Destination==str(config['GENERAL']['IpSink'])and init_config.GetIp(config['GENERAL']['StationInterface'])==str(config['GENERAL']['IpSink'])):
-    print("Data Received from: ", s)
+    if (packet.Destination==str(config['GENERAL']['IpSink'])and init_config.GetIp(config['GENERAL']['StationInterface'])==str(config['GENERAL']['IpSink'])):
+    print("GIUSEPPEEEEEEEEEEEEEEEEE---------------->Data Received from: ", s)
     data = packet.getBytesFromPackets()
     #data = packet.getBytesFromPackets()
     #UDP_Socket.SendUdpPacketUnicast(data, config['GENERAL']['IpController'], int(config['GENERAL']['PortController']))
